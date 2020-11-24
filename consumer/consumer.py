78cdd478bbd632edc6937bdd310fc59c96a418dc
James Jesudason <james.jesudason@gmail.com>
2020-11-24 15:28:47 +0000

    Tweaks to the provider and consumer

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -14,13 +14,6 @@ def client(mes):
     try:
         proxy = bus.get_object(ECHO_BUS_NAME, ECHO_OBJECT_PATH)
     except dbus.DBusException as e:
-        # There are actually two exceptions thrown:
-        # 1: org.freedesktop.DBus.Error.NameHasNoOwner
-        #   (when the name is not registered by any running process)
-        # 2: org.freedesktop.DBus.Error.ServiceUnknown
-        #   (during auto-activation since there is no .service file)
-        # TODO figure out how to suppress the activation attempt
-        # also, there *has* to be a better way of managing exceptions
         if e._dbus_error_name != 'org.freedesktop.DBus.Error.ServiceUnknown':
             raise
         if e.__context__._dbus_error_name != 'org.freedesktop.DBus.Error.NameHasNoOwner':
--- provider/provider.py
+++ provider/provider.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python3
 
-# standard includes
-import sys
-
-# dbus includes
 import gi.repository.GLib
 import dbus
 import dbus.service
@@ -16,32 +12,23 @@ ECHO_INTERFACE = 'com.hello.interface'
 
 class EchoServerObject(dbus.service.Object):
 
-    # TODO it would be nice to make a better decorator using annotations:
-    #   def foo(self, a: 's', b: 's') -> '': pass
-    # but the existing dbus decorator does its own reflection which
-    # fails if there are any annotations (or keyword-only arguments)
+    def __init__(self):
+        bus_name = dbus.service.BusName(ECHO_BUS_NAME, bus=dbus.SessionBus())
+        dbus.service.Object.__init__(self, bus_name, ECHO_OBJECT_PATH)
+
+
     @dbus.service.method(ECHO_INTERFACE,
                          in_signature='s', out_signature='')
     def echo(self, message):
-        message = str(message)  # get rid of subclass for repr
+        message = str(message)
         print('server: a client said %r' % message)
 
 
 def server():
     dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
 
-    bus = dbus.SessionBus()
-    try:
-        name = dbus.service.BusName(ECHO_BUS_NAME, bus, do_not_queue=True)
-    except dbus.NameExistsException:
-        sys.exit('Server is already running.')
-    else:
-        print('Server is not running yet. Putting on listening ears.')
-    echo = EchoServerObject(bus, ECHO_OBJECT_PATH)
-
-    mainloop = gi.repository.GLib.MainLoop()
-    echo.mainloop = mainloop
-    mainloop.run()
+    service = EchoServerObject()
+    gi.repository.GLib.MainLoop().run()
 
 
 if __name__ == '__main__':
